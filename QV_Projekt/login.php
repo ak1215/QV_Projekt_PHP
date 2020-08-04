d061564064af164e9b2d39d7d025b1446b54add0
Simge-GitHub <60422544+Simge-GitHub@users.noreply.github.com>
2020-08-04 08:11:22 +0200

    Update login.php

---
+++ QV_Projekt/login.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/www/QV-Rechner/stylesheet.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-sm-3"></div>
+      <div class="col-sm-6">
+            <h1 class="title">QV-Rechner</h1>
+            <h5 class="descr">Focus more on learning than on grades.</h5> 
+             
+            <div class="form-group">
+                <label class="form-check-label" for="username">Username:</label>
+                <input class="form-control" name="username" id="username" maxlength="100" required autofocus>
+            </div>
+            <div class="form-group">
+                <label class="form-check-label" for="password">Password:</label>
+                <input class="form-control" name="password" id="password" maxlength="100" required>
+            </div>
+            <button class="btn btn-secondary">Login</button>
+            <p class="text-signup">Don't have an account? Sign up now.</p>
+            <button class="btn btn-secondary">Sign Up</button>
+        </div>
+        <div class="col-sm-3"></div>
+
+    </div>
+</body>
